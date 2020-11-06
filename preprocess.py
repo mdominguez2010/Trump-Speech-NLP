d34a25349b5f2d604b39f1e27ea74b75dfd81732
Marcos Dominguez <mdominguez2010@yahoo.com>
2020-11-06 13:42:17 -0800

    mod functions

--- preprocess.py
+++ preprocess.py
@@ -14,32 +14,41 @@ from sklearn.model_selection import train_test_split
 
 # Clean text so it's ready for preprocessing
 
-def clean(speech):
+def clean(corpus):
     """
     Takes a speech as an argument and cleans it so it's ready to be pre-processed
     """
     # Initiate clean_corpus
+    clean_corpus = [] 
     
-    # Removes meaningless intro    
-    speech = speech[5:] 
+    for speech in corpus:
+    
+        # Removes meaningless intro    
+        speech = speech[5:] 
 
-    for i in range(len(speech)):
-        # Removes 'meaningless text hear (min:sec)\n' at the beginning of each paragraph
-        speech[i] = speech[i][speech[i].find('\n') + 1:] 
-        # Replaces brackets with paranthesis
-        speech[i] = speech[i].replace('[', '(') 
-        speech[i] = speech[i].replace(']', ')')
-        # Removes meaningless text in parantheses
-        speech[i] = re.sub(r'\([^)]*\)', '', speech[i]) 
+        for i in range(len(speech)):
+            # Removes 'meaningless text hear (min:sec)\n' at the beginning of each paragraph
+            speech[i] = speech[i][speech[i].find('\n') + 1:] 
+            # Replaces brackets with paranthesis
+            speech[i] = speech[i].replace('[', '(') 
+            speech[i] = speech[i].replace(']', ')')
+            # Removes meaningless text in parantheses
+            speech[i] = re.sub(r'\([^)]*\)', '', speech[i]) 
+
+        # Join all of the paragraphs into one speech
+        speech = ','.join(speech) 
+
+        clean_corpus.append(speech)
+    
+    # Combined all of the speeches into one huge document
+    clean_corpus = clean_corpus[0] + clean_corpus[1] + clean_corpus[2] + clean_corpus[3] + clean_corpus[4] + \
+                   clean_corpus[5] + clean_corpus[6] + clean_corpus[7]
         
-    # Join all of the paragraphs into on huge string
-    speech = ','.join(speech) 
-        
-    return speech
+    return clean_corpus
 
 # Tokenize corpus and preprocess text
 
-def preprocess(clean_speech):
+def preprocess(clean_corpus):
     """
     Takes in a speech, tokenizes and breaks it out into sentences, lemmatizes and removes stop words and punctuation for each speech
     """
@@ -58,16 +67,16 @@ def preprocess(clean_speech):
     
     # Tokenizer
     tokenizer = spacy.load('en_core_web_sm')
-    speech_tokens = tokenizer(clean_speech)
+    corpus_tokens = tokenizer(clean_corpus)
 
-    for sentence in speech_tokens.sents:
+    for sentence in corpus_tokens.sents:
         
         # Lemmatizing each token and converting each token into lowercase
         sentence = [word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in sentence]
         
         # Remove stop words, weird words and punctuations
         sentence = [word for word in sentence if word not in stop_words and word not in punctuations \
-                    and word not in weird_words]
+                         and word not in weird_words]
 
         # Remove numbers
         sentence = [word for word in sentence if word.isalpha()]
